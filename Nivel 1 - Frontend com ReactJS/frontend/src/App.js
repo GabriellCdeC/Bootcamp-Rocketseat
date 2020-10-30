17d8f600e203cb6406f8ca680b1a70b200fdd43d
Gabriell Castro de Carvalho <49693579+GabriellCdeC@users.noreply.github.com>
2020-10-30 14:14:42 -0300

    Finish module

--- Nivel 1 - Frontend com ReactJS/frontend/src/App.js
+++ Nivel 1 - Frontend com ReactJS/frontend/src/App.js
@@ -1,37 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from './components/Header'
 
+import api from './services/api'
 import './App.css'
-import backgroundImage from './assets/oliver-niblett-wh-7GeXxItI-unsplash.jpg'
 
 
 function App() {
 
+    const [projects, setProjects] = useState([])
 
+    useEffect(() => {
+        api.get('/projects').then(response => {
+            setProjects(response.data)
+        })
+    }, [])
 
-    function handleAddProject(){
-    
-        setProjects([...projects, `Novo Projeto ${Date.now()}`])
+    async function handleAddProject() {
 
+       const response = await api.post('/projects',{
+                    title: `Novo Projeto ${Date.now()}`,
+                    owner: "Gabriell Castro"
+                })
+            const project = response.data
+
+            setProjects([...projects, project])
     }
-
-    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web'])
 
     return (
         <>
             <Header title="Projects" />
 
-            <img width="300"  src={backgroundImage} alt="background image"/>
 
             <ul>
                 {
-                    projects.map(project => <li key={project}>{project}</li> )
+                    projects.map(project => <li key={project.id}>{project.title}</li>)
                 }
             </ul>
 
             <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
         </>
-        
+
     )
 }
 
