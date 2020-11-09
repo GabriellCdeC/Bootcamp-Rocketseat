213bb3c67cc05beba31608304a3edddc7806503f
Gabriell Castro de Carvalho <49693579+GabriellCdeC@users.noreply.github.com>
2020-11-09 19:20:52 -0300

    update day content

--- Semana 2/primeiro-projeto-node/src/middlewares/ensureAuthenticated.ts
+++ Semana 2/primeiro-projeto-node/src/middlewares/ensureAuthenticated.ts
@@ -3,21 +3,35 @@ import { verify } from 'jsonwebtoken'
 
 import authConfig from '../config/auth'
 
+interface TokenPayload {
+    iat: number,
+    exp: number,
+    sub: string
+}
+
 
 export default function ensureAuthenticated(request: Request, response: Response, next: NextFunction): void {
     const authHeader = request.headers.authorization
 
-    if(!authHeader){
+    if (!authHeader) {
         throw new Error('JWT token is missing')
     }
 
-    const  [ , token] = authHeader.split(' ')
+    const [, token] = authHeader.split(' ')
 
-    try{
+    try {
         const decoded = verify(token, authConfig.jwt.secret)
+
+        const { sub } = decoded as TokenPayload
+
+        request.user = {
+            id: sub
+        }
+
+        return next()
     } catch {
         throw new Error('Invalid JWT token')
     }
 
-    
+
 } 
